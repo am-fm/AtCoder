9898f9f6802b574abb7533b629dc13e4fa08fa54
am-fm <83386849+am-fm@users.noreply.github.com>
2021-06-09 20:43:19 +0900

    Accepted

---
+++ ABC006/ABC006_c++/A-世界のFizzBuzz.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+
+  if(n % 3){
+    cout << "NO";
+  }else{
+    cout << "YES";
+  }
+
+  cout << endl;
+  return 0;
+}
