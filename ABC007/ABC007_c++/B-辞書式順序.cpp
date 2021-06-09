f3ec3d214f04a395c6429e7920dd4f39fa9420bf
am-fm <83386849+am-fm@users.noreply.github.com>
2021-06-09 20:43:29 +0900

    Accepted

---
+++ ABC007/ABC007_c++/B-辞書式順序.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  string a;
+  cin >> a;
+
+  if(a == "a"){
+    cout << -1;
+  }else{
+    cout << 'a';
+  }
+
+  cout << endl;
+  return 0;
+}
